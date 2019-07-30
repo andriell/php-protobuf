310e877e24d269906033afe76422706b05d13bbb
andriell <root.andriell@gmail.com>
2019-07-30 18:41:49 +0300

    v1.0.0

---
+++ bin/pbf2json.php
@@ -0,0 +1,83 @@
+<?php
+
+use Protobuf\Pbf;
+use Protobuf\ProtocolBuffers;
+use Protobuf\StringReader;
+
+if (PHP_INT_SIZE != 8) {
+    echo 'This is only for x64 systems';
+    exit(1);
+}
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+$messages = require __DIR__ . '/../src/osm_messages.php';
+
+if (!(isset($argv[1]) && isset($argv[2]))) {
+    echo 'First argument is path to pbf file, second argument is dir to save';
+    exit(1);
+}
+$filePbf = $argv[1];
+if (!is_file($filePbf)) {
+    echo 'Is not a file: ' . $filePbf;
+    exit(1);
+}
+$saveDir = $argv[2];
+if (!is_dir($saveDir)) {
+    echo 'Is not a dir: ' . $saveDir;
+    exit(1);
+}
+
+$handle = fopen($filePbf, 'r');
+$fileSize = filesize($filePbf);
+
+
+$headerBlockCount = 0;
+$primitiveBlockCount = 0;
+$position = 0;
+$GLOBALS['start_time'] = time();
+
+while (!feof($handle)) {
+    $lengthBin = fread($handle, 4);
+    $position += 4;
+
+    $lengthInt = hexdec(bin2hex($lengthBin));
+    if ($lengthInt == 0) {
+        break;
+    }
+
+    $blobHeaderBin = fread($handle, $lengthInt);
+    $position += $lengthInt;
+
+    $blobHeaderPb = new ProtocolBuffers(new StringReader($blobHeaderBin), $messages);
+    $blobHeader = $blobHeaderPb->parse('BlobHeader');
+
+    $blobBin = fread($handle, $blobHeader['datasize']);
+    $position += $blobHeader['datasize'];
+
+    $blobPb = new ProtocolBuffers(new StringReader($blobBin), $messages);
+    $blob = $blobPb->parse('Blob');
+
+    $data = $blob['zlib_data'];
+
+    if (empty($blob['raw'])) {
+        $data = zlib_decode($data);
+    }
+    if ($blobHeader['type'] == 'OSMHeader') {
+        $headerBlockCount++;
+        $headerBlockPb = new ProtocolBuffers(new StringReader($data), $messages);
+        $headerBlock = $headerBlockPb->parse('HeaderBlock');
+        file_put_contents($saveDir . '/HeaderBlock' . $headerBlockCount . '.json', json_encode($headerBlock));
+    } elseif ($blobHeader['type'] == 'OSMData') {
+        $primitiveBlockCount++;
+        $primitiveBlockPb = new ProtocolBuffers(new StringReader($data), $messages);
+        $primitiveBlock = $primitiveBlockPb->parse('PrimitiveBlock');
+        file_put_contents($saveDir . '/PrimitiveBlock' . $primitiveBlockCount . '.json', json_encode($primitiveBlock));
+    } else {
+        echo 'Error: undefined BlobHeader type ' . $blobHeader['type'] . "\n";
+    }
+    StringReader::echoListener($position, $fileSize);
+}
+
+echo 'End' . "\n";
+echo 'Memory peak usage: ' . Pbf::formatBytes(memory_get_peak_usage()) . "\n";
